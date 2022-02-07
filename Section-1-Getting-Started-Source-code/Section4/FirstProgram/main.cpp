27fcb194e3f8c26c1bc35533288e85c1302244dc
iamshivay <shivaysabharwal.cse@gmail.com>
2022-02-07 19:40:48 +0530

    Added Section 1, and VSCode Conf.

---
+++ Section-1-Getting-Started-Source-code/Section4/FirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int favorite_number;
+    
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favorite_number;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    
+    return 0;
+}
