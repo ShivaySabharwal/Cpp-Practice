910a0bd1398c180bbae482cb4724bc6e1fbfe886
iamshivay <shivaysabharwal.cse@gmail.com>
2022-02-08 10:15:11 +0530

    Modified

--- Section-2-Structure-of-a-Cpp-Program-Source-code/Section5/BasicIO/main.cpp
+++ Section-2-Structure-of-a-Cpp-Program-Source-code/Section5/BasicIO/main.cpp
@@ -5,56 +5,54 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     cout << "Hello world!";
-    
-//    cout << "Hello";
-//    cout << "World" << endl;
 
-//    cout << "Hello world!" << endl;
-//    cout << "Hello" << " world!" << endl;
-//    cout << "Hello" << " world!\n";
-//    cout << "Hello\nOut\nThere\n";
+    cout << "Hello";
+    cout << "World" << endl;
 
-//    int num1;
-//    int num2;
-//    double num3;
+    cout << "Hello world!" << endl;
+    cout << "Hello"
+         << " world!" << endl;
+    cout << "Hello"
+         << " world!\n";
+    cout << "Hello\nOut\nThere\n";
 
-//    cout <<  "Enter an integer: ";
-//    cin >> num1;
-//    cout << "You entered: " << num1 << endl;
+    int num1;
+    int num2;
+    double num3;
 
-//    cout << "Enter a first integer: ";
-//    cin >> num1;
-//    
-//    cout << "Enter a second integer: ";
-//    cin >> num2;
-//    
-//    cout << "You entered " << num1 << " and " << num2 << endl;
-    
-    
-//    cout << "Enter 2 integers separated with a space: ";
-//    cin >> num1 >> num2;
-//    cout << "You entered " << num1 << " and " << num2 << endl;
+    cout << "Enter an integer: ";
+    cin >> num1;
+    cout << "You entered: " << num1 << endl;
 
+    cout << "Enter a first integer: ";
+    cin >> num1;
 
+    cout << "Enter a second integer: ";
+    cin >> num2;
 
-//    cout << "Enter a double: ";
-//    cin >> num3;
-//    
-//    cout << "You entered : " << num3 << endl;
+    cout << "You entered " << num1 << " and " << num2 << endl;
 
-   
-//    cout << "Enter an integer: ";
-//    cin >> num1;
-//    
-//    cout << "Enter a double: ";
-//    cin >> num3;
-//    
-//    cout << "The integer is: " << num1 << endl;
-//    cout << "And the double is " << num3 << endl;
-  
+    cout << "Enter 2 integers separated with a space: ";
+    cin >> num1 >> num2;
+    cout << "You entered " << num1 << " and " << num2 << endl;
+
+    cout << "Enter a double: ";
+    cin >> num3;
+
+    cout << "You entered : " << num3 << endl;
+
+    cout << "Enter an integer: ";
+    cin >> num1;
+
+    cout << "Enter a double: ";
+    cin >> num3;
+
+    cout << "The integer is: " << num1 << endl;
+    cout << "And the double is " << num3 << endl;
 
     return 0;
 }
