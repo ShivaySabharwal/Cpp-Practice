731667cb8c63df67c96509e74c7dc604c2e982eb
iamshivay <shivaysabharwal.cse@gmail.com>
2022-02-20 21:57:13 +0530

    Updation

--- Section-9-Pointers-and-References-Source-code/Section12/PassingPointers3/main.cpp
+++ Section-9-Pointers-and-References-Source-code/Section12/PassingPointers3/main.cpp
@@ -7,32 +7,34 @@
 
 using namespace std;
 
-void display( const vector<string> *const  v) {
-//(*v).at(0) = "Funny";
-    for (auto str: *v)
+void display(const vector<string> *const v)
+{
+    //(*v).at(0) = "Funny";
+    for (auto str : *v)
         cout << str << " ";
     cout << endl;
-  
-//v = nullptr;
+
+    // v = nullptr;
 }
 
-void display(const int *array, int sentinel) {
+void display(const int *array, int sentinel)
+{
     while (*array != sentinel)
         cout << *array++ << " ";
     cout << endl;
 }
 
-int main() {
+int main()
+{
 
     cout << "-----------------------------" << endl;
-    vector<string> stooges {"Larry", "Moe", "Curly"};
+    vector<string> stooges{"Larry", "Moe", "Curly"};
     display(&stooges);
-   
+
     cout << "\n-----------------------------" << endl;
-    int scores[] {100,98,97,79,85,-1};
+    int scores[]{100, 98, 97, 79, 85, -1};
     display(scores, -1);
-    
-    cout  << endl;
+
+    cout << endl;
     return 0;
 }
-
---
+++ Section-9-Pointers-and-References-Source-code/Section12/PassingPointers3/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+id display(const int *array, int sentinel) {
+//     while (*array != sentinel)
+//         cout << *array++ << " ";
+//     cout << endl;
+// }
