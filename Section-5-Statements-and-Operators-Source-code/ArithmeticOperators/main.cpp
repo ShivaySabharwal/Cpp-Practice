a6dcb388a7df66ebd7e685cf202c139b172c072b
iamshivay <shivaysabharwal.cse@gmail.com>
2022-07-02 18:12:18 +0530

    Modified Section - 5

--- Section-5-Statements-and-Operators-Source-code/ArithmeticOperators/main.cpp
+++ Section-5-Statements-and-Operators-Source-code/ArithmeticOperators/main.cpp
@@ -35,9 +35,6 @@ int main()
   result = num1 / num2;
   cout << num1 << " / " << num2 << " = " << result << endl;
 
-  result = num2 / num1;
-  cout << num2 << " / " << num1 << " = " << result << endl;
-
   result = num1 % num2;
   cout << num1 << " % " << num2 << " = " << result << endl;
 
--- Section-5-Statements-and-Operators-Source-code/IncrementDecrementOperators/main.cpp
+++ Section-5-Statements-and-Operators-Source-code/IncrementDecrementOperators/main.cpp
@@ -34,24 +34,24 @@ int main()
     cout << "Counter : " << counter << endl;
 
     // Example 2 - preincrement
-    //    counter = 10;
-    //    result = 0;
-    //
-    //    cout << "Counter : " << counter << endl;
-    //
-    //    result = ++counter; // Note the pre increment
-    //    cout << "Counter : " << counter << endl;
-    //    cout << "Result : " << result << endl;
+    counter = 10;
+    result = 0;
+
+    cout << "Counter : " << counter << endl;
+
+    result = ++counter; // Note the pre increment
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
     // Example 3 - post-increment
-    //    counter = 10;
-    //    result = 0;
-    //
-    //    cout << "Counter : " << counter << endl;
-    //
-    //    result = counter++; // Note the post increment
-    //    cout << "Counter : " << counter << endl;
-    //    cout << "Result : " << result << endl;
+    counter = 10;
+    result = 0;
+
+    cout << "Counter : " << counter << endl;
+
+    result = counter++; // Note the post increment
+    cout << "Counter : " << counter << endl;
+    cout << "Result : " << result << endl;
 
     // Example 4
     //    counter = 10;
