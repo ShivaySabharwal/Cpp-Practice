ba6bfe02960b982869b613f8ca5b598fbeda499f
Shivay Sabharwal <shivaysabharwal.cse@gmail.com>
2022-09-05 17:45:44 +0100

    Few Deletions

--- Section-10-OOPClasses-and-Objects-Source-code/MoveConstructor/main.cpp
+++ Section-10-OOPClasses-and-Objects-Source-code/MoveConstructor/main.cpp
@@ -57,12 +57,10 @@ int main() {
     vec.push_back(Move{20});
     vec.push_back(Move{30});
     vec.push_back(Move{40});
-     vec.push_back(Move{50});
+    vec.push_back(Move{50});
     vec.push_back(Move{60});
     vec.push_back(Move{70});
     vec.push_back(Move{80});
 
-
     return 0;
 }
-
