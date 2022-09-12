84ccfa343dfc1c0f8c74518b8fbde8dd8b45939f
Shivay Sabharwal <shivaysabharwal.cse@gmail.com>
2022-09-12 22:19:15 +0530

    Subtle Changes

--- Section-13-Polymorphism/VirtualDestructors/main.cpp
+++ Section-13-Polymorphism/VirtualDestructors/main.cpp
@@ -4,61 +4,65 @@
 #include <iostream>
 
 // This class uses dynamic polymorphism for the withdraw method
-class Account {
+class Account
+{
 public:
-    virtual void withdraw(double amount) {
+    virtual void withdraw(double amount)
+    {
         std::cout << "In Account::withdraw" << std::endl;
     }
     virtual ~Account() { std::cout << "Account:: destructor" << std::endl; }
 };
 
-class Checking: public Account  {
+class Checking : public Account
+{
 public:
-    virtual void withdraw(double amount) {
+    virtual void withdraw(double amount)
+    {
         std::cout << "In Checking::withdraw" << std::endl;
     }
     virtual ~Checking() { std::cout << "Checking:: destructor" << std::endl; }
-
 };
 
-class Savings: public Account  {
+class Savings : public Account
+{
 public:
-    virtual void withdraw(double amount) {
+    virtual void withdraw(double amount)
+    {
         std::cout << "In Savings::withdraw" << std::endl;
     }
     virtual ~Savings() { std::cout << "Savings:: destructor" << std::endl; }
-
 };
 
-class Trust: public Account  {
+class Trust : public Account
+{
 public:
-    virtual void withdraw(double amount) {
+    virtual void withdraw(double amount)
+    {
         std::cout << "In Trust::withdraw" << std::endl;
     }
     virtual ~Trust() { std::cout << "Trust:: destructor" << std::endl; }
-
 };
 
-int main() {
+int main()
+{
 
     std::cout << "\n === Pointers ==== " << std::endl;
     Account *p1 = new Account();
     Account *p2 = new Savings();
     Account *p3 = new Checking();
     Account *p4 = new Trust();
-    
+
     p1->withdraw(1000);
     p2->withdraw(1000);
     p3->withdraw(1000);
     p4->withdraw(1000);
-    
 
     std::cout << "\n === Clean up ==== " << std::endl;
     delete p1;
     delete p2;
     delete p3;
     delete p4;
-        
+
     return 0;
 }
-
